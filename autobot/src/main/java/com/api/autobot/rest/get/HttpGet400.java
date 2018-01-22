ed0b5033bd7a2acf729dae240ccba4a3573ac995
Vamsi Ravi <ravivamsi@yahoo.com>
2018-01-21 23:57:09 -0600

    Working on the 206 and 400 GET

---
+++ autobot/src/main/java/com/api/autobot/rest/get/HttpGet400.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.api.autobot.rest.get;
+
+/**
+ * @author vamsiravi
+ *
+ */
+public class HttpGet400 {
+
+}
---
+++ autobot/src/main/java/com/api/autobot/rest/get/HttpGet400Model.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package com.api.autobot.rest.get;
+
+import org.json.simple.JSONObject;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import io.restassured.response.Response;
+
+/**
+ * @author vamsiravi
+ *
+ */
+public class HttpGet400Model {
+	
+	
+	ExtentReports extentReports;
+	
+	ExtentTest test;
+	
+	Response response;
+	
+	JSONObject jsonResponseObject;
+
+	/**
+	 * @return the jsonResponseObject
+	 */
+	public JSONObject getJsonResponseObject() {
+		return jsonResponseObject;
+	}
+
+	/**
+	 * @param jsonResponseObject the jsonResponseObject to set
+	 */
+	public void setJsonResponseObject(JSONObject jsonResponseObject) {
+		this.jsonResponseObject = jsonResponseObject;
+	}
+
+	/**
+	 * @return the extentReports
+	 */
+	public ExtentReports getExtentReports() {
+		return extentReports;
+	}
+
+	/**
+	 * @param extentReports the extentReports to set
+	 */
+	public void setExtentReports(ExtentReports extentReports) {
+		this.extentReports = extentReports;
+	}
+
+	/**
+	 * @return the test
+	 */
+	public ExtentTest getTest() {
+		return test;
+	}
+
+	/**
+	 * @param test the test to set
+	 */
+	public void setTest(ExtentTest test) {
+		this.test = test;
+	}
+
+	/**
+	 * @return the response
+	 */
+	public Response getResponse() {
+		return response;
+	}
+
+	/**
+	 * @param response the response to set
+	 */
+	public void setResponse(Response response) {
+		this.response = response;
+	}
+	
+	
+	
+
+
+
+}
